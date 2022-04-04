f0468f3a5b8753c59ade3f238472da5d2ba7958c
aclassiter <a.class.wa@gmail.com>
2022-04-04 04:52:26 -0700

    completed assessment

--- coding-concepts.js
+++ coding-concepts.js
@@ -1,49 +1,63 @@
-// ASSESSMENT 2: Coding Conceptual Questions
+// // ASSESSMENT 2: Coding Conceptual Questions
 
-// Examine the following examples.
+// // Examine the following examples.
 
-// First, predict the outcome based on your understanding of the code.
-// Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
+// // First, predict the outcome based on your understanding of the code.
+// // Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
 
-// --------------------1) What will this log?
+// // --------------------1) What will this log?
 
 var cohort = "Bravo 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer:
+// This will log the same string with every letter in its own array. This will look like B,r,a,v,o,2,0,2,2
+// // b) Verify and explain:
+//Actual output: 
+//[
+//'B', 'r', 'a', 'v',
+//   'o', ' ', '2', '0',
+//   '2', '2'
+// ]
+// The actual output shows single quotation marks around each letter, with square brackets surrounding the entire array. My answer did not include these. The brackets indicate that the string letters have been split into an array. 
 
 
-// --------------------2) What will this log?
-
+// // --------------------2) What will this log?
 const greeter = (name) => {
-  `Hello, ${name}!`
+   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student")) 
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer:
+// This will not log anything. 
+
+// // b) Verify and explain:
+// This did not log anything, because there is no return statement on line 27. If there were a return statement, the output would read 'Hello, LEARN Student!'
 
 
-// --------------------3) What will this log?
+// // --------------------3) What will this log?
 
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+ console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer:
+// This will log [8,10,12,14,16]
+// // b) Verify and explain:
+// This does log [8,10,12,14,16] because the .map method returns an array with the same amount of numbers, and value * 2 multiplies every number by 2. 
 
 
-// --------------------4) What will this log?
+// // --------------------4) What will this log?
 
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
-// console.log(onlyOdds)
+ console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer:
+// This will log [11, 13, 15]
+// // b) Verify and explain:
+// This logs [11, 13, 15] because the .filter method filters out numbers based on a given condition, and the given condition is that odd numbers are filtered. This is why we see only odd numbers in the returned array. Value % 2 !== 0 means we are looking for numbers that are odd or rather, numbers that are not evenly divisible by 2. 
 
 
-// --------------------5) What will this log?
+// // --------------------5) What will this log?
 
 var myCodingSkills = {
   languages: ["JavaScript", "Ruby"],
@@ -51,23 +65,31 @@ var myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer:
+// This will log JavaScript
+// // b) Verify and explain:
+//This logs JavaScript because console.log is referencing the index of 0 in the languages array, which is the JavaScript value. If JavaScript were the only word, this would return J. 
 
 
-// --------------------STRETCH: What will this log?
+// // --------------------STRETCH: What will this log?
 
-class Learn {
-  constructor(name){
-    this.student = name,
-    this.cohort = "Bravo",
-    this.year = 2022
-  }
-}
-var learnStudent = new Learn("George")
+// class Learn {
+//   constructor(name){
+//     this.student = name,
+//     this.cohort = "Bravo",
+//     this.year = 2022
+//   }
+// }
+// var learnStudent = new Learn("George")
 // console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer:
+// // b) Verify and explain:
+
+
+
+
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,49 +8,43 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-  Your answer:
+  Your answer: A parameter is defined by a function and is found within parentheses after the function name. An argument affects change within the function. 
 
-  Researched answer:
-
-
+  Researched answer: Where the function is invoked, arguments are found in parentheses after the function name, and serve to pass content to the function. For every argument, there is an equal number of paremeters. Arguments can be different data types such as an array of numbers, or strings. 
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: The predefined paremeters taken by the .map method are values that lie within an array. I do not recall which parameters are required and which are optional, but I will gladly do more research on this method and follow up with my findings. 
 
-  Researched answer:
-
+  Researched answer: The predefined parameters of the .map() built-in method are value, index, and array. The value is required, whereas the index and array are optional. 
 
 
 3. What is the difference between map and filter?
 
-  Your answer:
+  Your answer: Both map and filter are built-in methods for arrays. Map is used to return an array with the same number of elements (or values), whether altered or not, and filter is used to filter out certain elements (or values), returning an array containing fewer than it originated with. 
 
-  Researched answer:
-
+  Researched answer: The map method uses iteration through an array, and will return an updated array of the same length. The filter method also iterates through an array however, this method will only return an array containing values that meet a specified condition. 
 
 
 4. What is the difference between a function and a method?
 
-  Your answer:
+  Your answer: A function is code that works like a mini machine to execute actions defined by the developer. A method is a built-in function with a predefined role that can be called upon when needed, and can be used within a function. 
 
-  Researched answer:
-
+  Researched answer: A function defines a task, or set of tasks that can be executed over and over following detailed instructions. A method performs tasks similarly to a function however, a method is associated with an object while a function is not. 
 
 
 5. What is object destructuring?
 
-  Your answer:
+  Your answer: Destructuring assigns values individually within an array. 
 
-  Researched answer:
-
+  Researched answer: Destructuring allows for the existence of individual variables within the assignment of a single array. For example, an array containing information about cars may include make, model, and year. We could log these variables separately, with different values each time. 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: I do not recall the meaning of hoisting in JavaScript, but I am happy to look it up and get back to you. 
 
-  Researched answer:
+  Researched answer: Hoisting is the default behavior of JavaScript that allows all declarations to be moved to the top of their current function. Only declarations are hoisted in JavaScript, not initializations. 
 
 
 
@@ -58,9 +52,11 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. Class Inheritance:
+1. Class Inheritance: 
+Class inheritance involves a parent (generic) class, and child (specific) class relationship. The parent class contains attributes that are general in nature, and are passed down (inherited by) the child class. The child class can have its own properties in addition to those it receives from the parent class, which will not alter the original properties of the parent class. 
 
 2. React:
+React is a scalable programming language commonly used to build webpages and mobile applications. The structure of React is such that everything is a component, or function, and those components share characteristics that can be used in a similar way. 
 
 3. React state:
 
